be7a1334b8e2d606c86fb87bc72c7ccf43877c5a
euomauricio <50330236+euomauricio@users.noreply.github.com>
2022-02-16 21:41:09 -0300

    dia 1 fim da aula

--- Borracharia/js/busca-cep.js
+++ Borracharia/js/busca-cep.js
@@ -1,3 +1,4 @@
+/*
 async function buscaCep() {
     const cep = document.getElementById("input-cep").value
     const ht = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
@@ -6,6 +7,18 @@ async function buscaCep() {
     document.getElementById("bairro").innerHTML = json.bairro
     document.getElementById("rua").innerHTML = json.logradouro
     document.getElementById("cidade").innerHTML = json.localidade
-
+    console.log(json);
 }
 document.getElementById("botao").addEventListener("click", buscaCep)
+*/
+async function buscaCep() {
+    const cep = document.getElementById("input-cep").value;
+    const response = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+    document.getElementById('cidade').innerHTML = json.localidade;
+    document.getElementById('bairro').innerHTML = json.bairro;
+    document.getElementById('rua').innerHTML = json.logradouro;
+}
+document.getElementById('botao').addEventListener('click', buscaCep);
