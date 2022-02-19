c2efd4dbf1a237aabbd5037674033c46718effec
euomauricio <50330236+euomauricio@users.noreply.github.com>
2022-02-18 21:57:49 -0300

    atualizações aula dia 18

--- Borracharia/js/busca-cep.js
+++ Borracharia/js/busca-cep.js
@@ -14,11 +14,28 @@ document.getElementById("botao").addEventListener("click", buscaCep)
 async function buscaCep() {
     const cep = document.getElementById("input-cep").value;
     const response = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
-    console.log(response);
     const json = await response.json();
-    console.log(json);
-    document.getElementById('cidade').innerHTML = json.localidade;
-    document.getElementById('bairro').innerHTML = json.bairro;
-    document.getElementById('rua').innerHTML = json.logradouro;
+    let rua = json.logradouro;
+    let bairro = json.bairro;
+    let cidade = json.localidade;
+    let uf = json.uf;
+    document.getElementById('rua-input').innerHTML = `${rua}, ${bairro} - ${cidade} - ${uf}`;
+
+
+
+    if (cep >= 89010000 && cep <= 89099999) {
+        document.getElementById('response-true').innerHTML = `Que bom! Nós temos uma filial em ${json.localidade} c:`;
+    } else {
+        document.getElementById('response-false').innerHTML = "Ainda não temos uma filial na sua cidade :c";
+    }
+
+    /*
+
+    89010000
+    89100000
+
+    document.getElementById('rua-input').innerHTML = json.logradouro;
+    document.getElementById('bairro-input').innerHTML = json.bairro;
+    document.getElementById('cidade-input').innerHTML = json.localidade;*/
 }
-document.getElementById('botao').addEventListener('click', buscaCep);
+document.getElementById('searchbtn').addEventListener('click', buscaCep);
